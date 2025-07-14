f9b2658a63cf9301fb911fcc3dbdae210de0266d
ygdstmidn <2022d12@stu.gifu-nct.ac.jp>
2025-07-15 07:41:30 +0900

    ✨ 動くかはわからないが書いた

--- Core/Src/app.cpp
+++ Core/Src/app.cpp
@@ -1,42 +1,43 @@
 // main code here!!
 
 #ifdef __cplusplus
-extern "C"
-{
-#endif
 
 #include "main.h"
 #include "i2c.h"
 #include "usart.h"
 #include "gpio.h"
 
-    // MARK:setup
-    void user_setup(void)
-    {
-        setbuf(stdout, NULL);
-        printf("Hello World!!\n");
-    }
+#include <GP2Y0E03/GP2Y0E03.h>
 
-    // MARK:loop
-    void user_loop(void)
-    {
-        for (uint8_t addr = 1; addr < 128; addr++)
-        {
-            if (HAL_I2C_IsDeviceReady(&hi2c1, (uint16_t)(addr << 1), 1, 10) == HAL_OK)
-            {
-                printf("I2C device found at address 0x%02X\r\n", addr);
-            }
-        }
-        HAL_Delay(1000);
-    }
+GP2Y0E03::GP2Y0E03 myGPsensor(&hi2c1, 0x80);
 
-    // MARK:_write (for printf)
-    int _write(int file, char *ptr, int len)
-    {
-        HAL_UART_Transmit(&huart2, (uint8_t *)ptr, len, 10);
-        return len;
-    }
-
-#ifdef __cplusplus
+// MARK:setup
+void user_setup(void)
+{
+    setbuf(stdout, NULL);
+    printf("Hello World!!\n");
 }
+
+// MARK:loop
+void user_loop(void)
+{
+    const uint32_t now = HAL_GetTick();
+    static uint32_t pre = now;
+
+    if (now - pre >= 10)
+    {
+        const double distance = myGPsensor.read();
+        printf(">distance:%f", distance);
+
+        pre = now;
+    }
+}
+
+// MARK:_write (for printf)
+int _write(int file, char *ptr, int len)
+{
+    HAL_UART_Transmit(&huart2, (uint8_t *)ptr, len, 10);
+    return len;
+}
+
 #endif
